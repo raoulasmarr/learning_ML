33870e821f137bd9a9460ae0d43ca95f4c2b327f
raoul asmar <raoulasmar24@gmail.com>
2026-08-21 19:59:31 -0700

    worked on computer_vision.py

--- pytorch_basics/computer_vision.py
+++ pytorch_basics/computer_vision.py
@@ -4,7 +4,7 @@ from torch import nn
 import torchvision
 from torchvision import datasets
 from torchvision.transforms import ToTensor
-
+from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
 
 print(torch.__version__)
@@ -24,6 +24,8 @@ def main():
         transform=ToTensor()
     )
 
+
+    #Displaying an image and cheching image shape
     image, label = train_data[0]
     print(f"Train data shape: {train_data}")
     print(f"Image shape: {image.shape}, Label: {label}")
@@ -31,5 +33,23 @@ def main():
     print(f"Train data: {train_data.data}")
     plt.imshow(image.squeeze(), cmap="gray")
     plt.show()
+
+    # loading data into 32 batch size
+
+    BATCH_SIZE = 32
+
+    train_dataloader = DataLoader(train_data, batch_size = BATCH_SIZE, shuffle = True)
+    test_dataloader = DataLoader(test_data, batch_size = BATCH_SIZE, shuffle = False)
+
+    print(train_dataloader, test_dataloader)
+    print(len(train_dataloader))
+    print(  BATCH_SIZE )
+
+    #CHECK WHATS inside the data loaders
+    train_features_batch, train_labels_batch= next(iter(train_dataloader[1]))
+    print(train_features_batch, train_labels_batch)
+    print(train_features_batch.shape, train_labels_batch.shape)
+
+
 main()
  
